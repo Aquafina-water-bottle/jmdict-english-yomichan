a7d6df1d59de45109158f647399969fbe98afcfc
Austin Siew <17107540+Aquafina-water-bottle@users.noreply.github.com>
2023-02-11 00:50:17 -0700

    build command recorded

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-./yomichan -format edict -title "JMdict (English)" 2023-02-10/JMdict_e 2023-02-10_jmdict_english.zip
+./yomichan -format edict -title "JMdict (English)" 2023-02-10/JMdict_e 2023-02-11_jmdict_english.zip
+./yomichan -language=english_extra -format edict -title "JMdict (English)" 2023-02-10/JMdict_e_examp 2023-02-11_jmdict_english_extra.zip
